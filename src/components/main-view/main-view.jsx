770dff72b7c4f4d2dd0a9a02ff46b240143e9fbb
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-10-15 12:18:48 -0400

    updates
    update to movie-card and movie-main trying to get the movieClick to work

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -32,10 +32,8 @@ export class MainView extends React.Component {
       return (
         <div className="main-view">
           {selectedMovie
-            ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-            : movies.map(movie => (
-              <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-            ))
+              ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+              : movies.map(movie => (<MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} /> ))
           }
         </div>
       );
--- src/components/main-view/movie-card/movie-card.jsx
+++ src/components/main-view/movie-card/movie-card.jsx
@@ -5,6 +5,6 @@ export class MovieCard extends React.Component {
   render() {
       const { movie } = this.props;
 
-      return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title} </div>;  
+      return <div className="movie-card" onClick={() => {onMovieClick(movie); }}>{movie.Title}</div>    
     }
 }
