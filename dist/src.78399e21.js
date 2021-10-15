44261b8c1032aa1a842a2763e8e4be4503ee4354
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-10-15 12:50:50 -0400

    updates
    update main-view and movie-card

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29984,7 +29984,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63963" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54591" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
