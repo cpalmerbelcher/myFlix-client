39fe501bfa1604cdda7291eb6d269cd1a91e4b32
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-10-27 18:14:06 -0400

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33149,7 +33149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56764" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62008" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
