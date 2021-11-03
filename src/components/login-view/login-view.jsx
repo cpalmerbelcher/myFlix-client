c1e7173b357f66971ac7a0ddedc311d3374e3ed3
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-11-02 21:46:16 -0400

    updates
    installed React Bootstrap and updated styling across my app.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import PropTypes from "prop-types";
+import { Navbar, Nav, Form, Button, Card, Container } from 'react-bootstrap';
+
+import './login-view.scss';
 
 export function LoginView(props) {
     const [ username, setUsername ] = useState ('');
@@ -13,14 +17,55 @@ export function LoginView(props) {
     };
 
     return (
-        <form>
-            <label>
-                Username: <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <lable>
-                Password: <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </lable>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-        </form>
-    );
-}
+        <Container fluid className="loginContainer" >
+    
+          <Navbar bg="navColor" variant="dark" expand="lg">
+            <Container fluid>
+              <Navbar.Brand href="#home">My-Flix App</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="#logout">Login</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        
+          <Card className="loginCard">
+            <Card.Body>
+              <Card.Title className="text-center">Welcome to My-Flix App.</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted text-center">Please Login</Card.Subtitle>
+          
+              <Form >
+                <Form.Group controlId="formUsername">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control 
+                    type="text" 
+                    onChange={e => setUsername(e.target.value)}
+                  />
+                </Form.Group>
+    
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control 
+                    className="mb-3" 
+                    type="password" 
+                    onChange={e => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+    
+                <Button className="loginButton" variant="secondary" size="lg" type="submit" onClick={handleSubmit}>
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Container>
+      );
+        
+    }
+    
+    LoginView.propTypes = {
+      user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired,
+      }),
+      onLoggedIn: PropTypes.func.isRequired,
+    };
