886004a3455ef858efdb9bcbcd43f9daf62c0149
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2022-03-28 20:41:49 -0400

    Update app.module.ts

--- myFlix-Angular-client/src/app/app.module.ts
+++ myFlix-Angular-client/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatDialogModule,
